ea85a46c9c5e37abba941d7e0c4c297bc2536c34
Dima <dmitryberesten@gmail.com>
2022-06-20 10:43:20 +0300

    Обновление

---
+++ build/js/show-modal.js
@@ -0,0 +1,1 @@
+let windowModal=document.querySelector(".modal-window-wrapper"),orderButton=document.querySelector(".product-of-week__button");orderButton.removeAttribute("href"),orderButton.onclick=function(){windowModal.classList.remove("modal-window-wrapper--none")},windowModal.onclick=function(){windowModal.classList.add("modal-window-wrapper--none")};
